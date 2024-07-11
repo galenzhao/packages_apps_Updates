f4a22c20804aeeab02220853b29af00db62c7c14
Your Name <you@example.com>
2024-07-11 17:16:08 +0800

    change updater repo

--- app/src/main/java/net/pixelos/ota/misc/Utils.java
+++ app/src/main/java/net/pixelos/ota/misc/Utils.java
@@ -156,7 +156,7 @@ public class Utils {
     }
 
     public static String getServerURL() {
-        return String.format(Constants.OTA_URL, SystemProperties.get(Constants.PROP_BUILD_VERSION), SystemProperties.get(Constants.PROP_DEVICE));
+        return String.format(Constants.OTA_URL, "fourteen", SystemProperties.get(Constants.PROP_DEVICE));
     }
 
     public static String getMaintainerURL(String username) {
@@ -164,7 +164,7 @@ public class Utils {
     }
 
     public static String getDownloadWebpageUrl(String fileName) {
-        return String.format(Constants.DOWNLOAD_WEBPAGE_URL, SystemProperties.get(Constants.PROP_DEVICE));
+        return String.format(Constants.DOWNLOAD_WEBPAGE_URL, "files");
     }
 
     public static void triggerUpdate(Context context) {
